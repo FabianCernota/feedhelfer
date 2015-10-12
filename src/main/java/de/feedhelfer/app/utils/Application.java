ac8f99b96fbd6869888f1149549da790973b040f
Fabian Cernota <fabian.cernota@chamaeleon.de>
2015-10-12 14:38:10 +0200

    initial

---
+++ src/main/java/de/feedhelfer/app/utils/Application.java
@@ -0,0 +1,24 @@
+package de.feedhelfer.app.utils;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+
+    }
+
+}
