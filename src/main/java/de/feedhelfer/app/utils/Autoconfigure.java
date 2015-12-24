66b0a0eb00475414ac52001ec5f3ea6ba7fc009f
Fabian Cernota <fabian.cernota@chamaeleon.de>
2015-12-24 03:00:20 +0100

    Missing Classes and initial.sql

---
+++ src/main/java/de/feedhelfer/app/utils/Autoconfigure.java
@@ -0,0 +1,16 @@
+package de.feedhelfer.app.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+
+/**
+ * Created by cernota on 22.12.15.
+ */
+@Controller
+public class Autoconfigure {
+    public Autoconfigure(){
+         Logger log = LoggerFactory.getLogger(Autoconfigure.class);
+    }
+}
